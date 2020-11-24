f318bef36a2949935b31dc181464a762cbf13945
pengzai <roc.liu@hotmail.com>
2020-11-24 16:34:03 +0800

    测试

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 const core = require("@actions/core");
 // const github = require("@actions/github");
-var exec = require('child_process').exec;
+var exec = require("child_process").exec;
+var fs = require("fs");
 try {
   // `who-to-greet` input defined in action metadata file
   // const nameToGreet = core.getInput("config");
   // const address = core.getInput('address');
-  const projectType = core.getInput('project-type');
-  const deployType = core.getInput('deploy-type');
-  const ip = core.getInput('ip');
-  const username = core.getInput('username');
-  const password = core.getInput('username');
-  const projectPath = core.getInput('project-path');
-  if(!projectType||!deployType){
-    console.log('请传入project-type,project-type');
+  const projectType = core.getInput("project-type");
+  const deployType = core.getInput("deploy-type");
+  const ip = core.getInput("ip");
+  const username = core.getInput("username");
+  const password = core.getInput("username");
+  const projectPath = core.getInput("project-path");
+  if (!projectType || !deployType) {
+    console.log("请传入project-type,project-type");
     return;
   }
-  if(!ip||!username||!password){
-    console.log('请传入ip,usename,password');
+  if (!ip || !username || !password) {
+    console.log("请传入ip,usename,password");
     return;
   }
-  if(projectType=='web-build'){
-    exec("yarn install", function(error, stdout, stderr) {
+  if (projectType == "web-build") {
+    exec("yarn install", function (error, stdout, stderr) {
       // 获取命令执行的输出
-      console.log(error,stdout,stderr);
+      console.log(error, stdout, stderr);
+    });
+    exec("yarn build", function (error, stdout, stderr) {
+      // 获取命令执行的输出
+      console.log(error, stdout, stderr);
+    });
+    fs.writeFile("rsync.pass", password, function (err) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("数据写入成功！");
+      exec('chmod 600 rsync.pass', function (error, stdout, stderr) {
+        // 获取命令执行的输出
+        console.log(error, stdout, stderr);
+      });
+    });
+    exec(`rsync --password-file=rsync.pass ./build ${username}@${ip}::${projectPath}`, function (error, stdout, stderr) {
+      // 获取命令执行的输出
+      console.log(error, stdout, stderr);
     });
   }
   // const username = core.getInput('username');
