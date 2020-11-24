3a5b0fff012048c77c4f70541fc4eb6c875c406d
pengzai <roc.liu@hotmail.com>
2020-11-24 16:08:49 +0800

    测试

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
-
+// const github = require("@actions/github");
+var exec = require('child_process').exec;
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  // const nameToGreet = core.getInput("config");
+  // const address = core.getInput('address');
+  const projectType = core.getInput('project-type');
+  const deployType = core.getInput('deploy-type');
+  const ip = core.getInput('ip');
+  const username = core.getInput('username');
+  const password = core.getInput('username');
+  const projectPath = core.getInput('project-path');
+  if(!projectType||!deployType){
+    console.log('请传入project-type,project-type');
+    return;
+  }
+  if(!ip||!username||!password){
+    console.log('请传入ip,usename,password');
+    return;
+  }
+  if(projectType=='web-build'){
+    exec("yarn install", function(error, stdout, stderr) {
+      // 获取命令执行的输出
+      console.log(error,stdout,stderr);
+    });
+  }
+  // const username = core.getInput('username');
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
