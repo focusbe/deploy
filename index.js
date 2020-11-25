72fe835bbb18e95344c3ba46b7bb1478e88dfb1b
pengzai <roc.liu@hotmail.com>
2020-11-25 10:42:47 +0800

    测试

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const core = require("@actions/core");
 const build = require("./modules/build/index.js");
 const deploy = require("./modules/deploy");
 console.log(process.env);
-try {
-  const projectType = core.getInput("project-type");
-  await build(projectType);
-  await deploy();
-} catch (error) {
-  core.setFailed(error.message);
+async function main() {
+  try {
+    const projectType = core.getInput("project-type");
+    await build(projectType);
+    await deploy();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+main();
