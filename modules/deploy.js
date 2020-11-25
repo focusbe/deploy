4a565a6fef8b986b7e0e9eeb43f244fd55b822c6
pengzai <roc.liu@hotmail.com>
2020-11-25 18:42:14 +0800

    测试

--- modules/deploy.js
+++ modules/deploy.js
@@ -10,32 +10,36 @@ const remotePath = core.getInput("remote-path");
 const projectType = core.getInput("project-type");
 const deployType = core.getInput("deploy-type");
 const projectName = process.env.GITHUB_REPOSITORY.split("/").pop();
-
+const excludeFiles = [".git", ".github", ".vscode", "node_modules"];
 async function main(dist) {
   if (!deployType) {
     throw new Error("deploy-type should not be null");
   }
   var remotedir = `${remotePath}/${projectName}`;
+
   if (deployType == "rsync" || deployType == "ssh") {
     var passwordstr = "";
     var maohao = ":";
+    var pswfile = "../rsync.pass";
+    fs.writeFileSync(pswfile, password);
+    await Utli.runSh("chmod 600 " + pswfile);
     if (deployType == "rsync") {
       maohao = "::";
-      fs.writeFileSync("rsync.pass", password);
-      await Utli.runSh("chmod 600 rsync.pass");
-      passwordstr = "--password-file=rsync.pass";
+      passwordstr = `--password-file="${pswfile}"`;
     } else {
-      fs.writeFileSync("rsync.pass", password);
-      await Utli.runSh("chmod 600 rsync.pass");
-      passwordstr = "-e 'ssh -i rsync.pass -o StrictHostKeyChecking=no'";
+      passwordstr = `-e 'ssh -i ${pswfile} -o StrictHostKeyChecking=no'`;
+    }
+    var excludestr = "";
+    for (var i in excludeFiles) {
+      excludestr += ` --exclude "${excludeFiles[i]}"`;
     }
     //   //纯前端项目非增量同步
     var deletetag = projectType.indexOf("front-") == 0 ? "--delete" : "";
-    var rsynccmd = `rsync ${deletetag} -av ${passwordstr} --exclude ".*" --exclude rsync.pass --exclude "node_modules" ./${dist} ${username}@${ip}${maohao}${remotedir}`;
+    var rsynccmd = `rsync ${deletetag} -av ${passwordstr} ${excludestr} ./${dist} ${username}@${ip}${maohao}${remotedir}`;
     await Utli.runSh(rsynccmd);
   } else if (deployType == "ftp") {
-    console.log(process.env);
-    fs.writeFileSync("../rsync.pass", password);
+    // console.log(process.env);
+    // fs.writeFileSync("../rsync.pass", password);
     var config = {
       user: username,
       password: password,
@@ -44,9 +48,8 @@ async function main(dist) {
       localRoot: "./" + dist,
       include: ["*", "**/*"],
       remoteRoot: remotedir,
-      exclude: [".git", ".github/*", "node_modules/*"],
+      exclude: excludeFiles,
     };
-    console.log(config);
     await new ftpDeploy().deploy(config);
   }
   console.log(`deploy to ${ip} over ${deployType}`);
