58cc4dac85fc781a03017a6ee8fd0918233692ce
pengzai <roc.liu@hotmail.com>
2021-10-21 09:11:48 +0800

    增加参数支持自定义source path

--- index.js
+++ index.js
@@ -1,49 +1,67 @@
-const core = require("@actions/core");
-const build = require("./modules/build/index.js");
-const deploy = require("./modules/deploy");
-const Base64 = require("js-base64");
+const core = require('@actions/core');
+const build = require('./modules/build/index.js');
+const deploy = require('./modules/deploy');
+const Base64 = require('js-base64');
 global.Config = {};
 class Main {
-  constructor() {
-    this.init();
-  }
-  async init() {
-    try {
-      if (this.getConfig()) {
-        await build();
-        await deploy(global.Config["project-type"].indexOf("build") > -1 ? "dist/" : "");
-      }
-    } catch (error) {
-      console.error(error);
-      core.setFailed(error.message);
+    constructor() {
+        this.init();
     }
-  }
-  getConfig() {
-    const token = core.getInput("deploy-token");
-    if (token) {
-      try {
-        global.Config = JSON.parse(Base64.decode(token));
-      } catch (error) {
-        console.error("token parse error you can get token by https://tools.focusbe.com/deploy-token");
-        return false;
-      }
-    } else {
-      ["deploy-type", "project-type", "username", "password", "ip", "remote-path", "port","project-name"].map((item, key) => {
-        global.Config[item] = core.getInput(item);
-      });
+    async init() {
+        try {
+            if (this.getConfig()) {
+                await build();
+                await deploy(
+                    global.Config['project-type'].indexOf('build') > -1
+                        ? 'dist/'
+                        : ''
+                );
+            }
+        } catch (error) {
+            console.error(error);
+            core.setFailed(error.message);
+        }
     }
-    if (!global.Config) {
-      console.error("inputs can not be null");
-      return false;
+    getConfig() {
+        const token = core.getInput('deploy-token');
+        if (token) {
+            try {
+                global.Config = JSON.parse(Base64.decode(token));
+            } catch (error) {
+                console.error(
+                    'token parse error you can get token by https://tools.focusbe.com/deploy-token'
+                );
+                return false;
+            }
+        } else {
+            [
+                'deploy-type',
+                'project-type',
+                'username',
+                'password',
+                'ip',
+                'remote-path',
+                'port',
+                'project-name',
+                'args',
+                'source-path',
+                'exclude',
+            ].map((item, key) => {
+                global.Config[item] = core.getInput(item);
+            });
+        }
+        if (!global.Config) {
+            console.error('inputs can not be null');
+            return false;
+        }
+        var requiredParams = ['deploy-type', 'project-type', 'ip', 'username'];
+        for (var i in requiredParams) {
+            if (!global.Config[requiredParams[i]]) {
+                console.error(requiredParams[i] + ' can not be null');
+                return false;
+            }
+        }
+        return global.Config;
     }
-    var requiredParams = ["deploy-type", "project-type", "ip", "username"];
-    for (var i in requiredParams) {
-      if (!global.Config[requiredParams[i]]) {
-        console.error(requiredParams[i] + " can not be null");
-        return false;
-      }
-    }
-    return global.Config;
-  }
 }
 new Main();
--- modules/deploy.js
+++ modules/deploy.js
@@ -1,53 +1,56 @@
 //部署到服务器
-const ftpDeploy = require("ftp-deploy");
-const fs = require("fs");
-const Utli = require("./utli");
-const excludeFiles = [".git/**", ".github/**", ".vscode/**", "node_modules/**"];
+const ftpDeploy = require('ftp-deploy');
+const fs = require('fs');
+const Utli = require('./utli');
+const excludeFiles = ['.git/**', '.github/**', '.vscode/**', 'node_modules/**'];
 async function main(dist) {
-  const username = global.Config.username;
-  const password = global.Config.password;
-  const ip = global.Config.ip;
-  var remotePath = global.Config["remote-path"];
-  const projectType = global.Config["project-type"];
-  const deployType = global.Config["deploy-type"];
-  const projectName = global.Config["project-name"] || process.env.GITHUB_REPOSITORY.split("/").pop();
-  if (remotePath[remotePath.length - 1] != "/") {
-    remotePath += "/";
-  }
-  var remotedir = `${remotePath}${projectName}`;
-  if (deployType == "rsync" || deployType == "ssh") {
-    var passwordstr = "";
-    var maohao = ":";
-    var pswfile = "../rsync.pass";
-    fs.writeFileSync(pswfile, password);
-    await Utli.runSh("chmod 600 " + pswfile);
-    if (deployType == "rsync") {
-      maohao = "::";
-      passwordstr = `--password-file="${pswfile}"`;
-    } else {
-      passwordstr = `-e 'ssh -i ${pswfile} -o StrictHostKeyChecking=no -p ${global.Config.port}'`;
+    const username = global.Config.username;
+    const password = global.Config.password;
+    const ip = global.Config.ip;
+    var remotePath = global.Config['remote-path'];
+    const projectType = global.Config['project-type'];
+    const deployType = global.Config['deploy-type'];
+    const projectName =
+        global.Config['project-name'] ||
+        process.env.GITHUB_REPOSITORY.split('/').pop();
+    if (remotePath[remotePath.length - 1] != '/') {
+        remotePath += '/';
     }
-    var excludestr = "";
-    for (var i in excludeFiles) {
-      excludestr += ` --exclude ${excludeFiles[i].replace("/**", "")}`;
+    var remotedir = `${remotePath}${projectName}`;
+    if (deployType == 'rsync' || deployType == 'ssh') {
+        var passwordstr = '';
+        var maohao = ':';
+        var pswfile = '../rsync.pass';
+        fs.writeFileSync(pswfile, password);
+        await Utli.runSh('chmod 600 ' + pswfile);
+        if (deployType == 'rsync') {
+            maohao = '::';
+            passwordstr = `--password-file="${pswfile}"`;
+        } else {
+            passwordstr = `-e 'ssh -i ${pswfile} -o StrictHostKeyChecking=no -p ${global.Config.port}'`;
+        }
+        var excludestr = '';
+        for (var i in excludeFiles) {
+            excludestr += ` --exclude ${excludeFiles[i].replace('/**', '')}`;
+        }
+        var deletetag = '';
+        const sourceDir = global.Config['source-path'];
+        var rsynccmd = `rsync ${global.Config.args} ${passwordstr} ${global.Config.exclude} ./${sourceDir} ${username}@${ip}${maohao}${remotedir}`;
+        await Utli.runSh(rsynccmd);
+    } else if (deployType == 'ftp') {
+        var config = {
+            user: username,
+            password: password,
+            host: ip,
+            port: global.Config.port,
+            localRoot: './' + dist,
+            include: ['*', '**/*'],
+            remoteRoot: remotedir,
+            exclude: excludeFiles,
+        };
+        await new ftpDeploy().deploy(config);
     }
-    var deletetag = "";
-    var rsynccmd = `rsync ${deletetag} -av ${passwordstr} ${excludestr} ./${dist} ${username}@${ip}${maohao}${remotedir}`;
-    await Utli.runSh(rsynccmd);
-  } else if (deployType == "ftp") {
-    var config = {
-      user: username,
-      password: password,
-      host: ip,
-      port: global.Config.port,
-      localRoot: "./" + dist,
-      include: ["*", "**/*"],
-      remoteRoot: remotedir,
-      exclude: excludeFiles,
-    };
-    await new ftpDeploy().deploy(config);
-  }
-  console.log(`deploy to ${ip} over ${deployType} fineshed`);
+    console.log(`deploy to ${ip} over ${deployType} fineshed`);
 }
 
 module.exports = main;
